d0a89d8575b9dcd571d239a93324e10aeee7b816
ljx_glb <1913154305@qq.com>
2019-03-02 11:15:50 +0800

    msc version2.03
    complete the function push and msc
    but there is no border when you push the mouse.
    
    on 2st March 2019 11:15

--- msc.js
+++ msc.js
@@ -1,97 +1,101 @@
 window.onload = function () {
 
-    const body = document.body;
     const canvas = document.getElementById('msc');
-    const textArea = document.getElementById('urls');
-    let  source = 'image/face.jpg';
-
+    const source = 'image/face.jpg';
 
     if (canvas.getContext){
         var  size = 5;
 
-        console.log(source);
         var ctx = canvas.getContext('2d');
+        ctx.save();
+
+        var image= new Image();
+
+        image.src = source;
+
+        image.onload = function () {
+
+            canvas.width = 500;
+            canvas.height = 500;
+            ctx.drawImage(image, 100, 100);
 
 
-
-            body.onmousedown = function (ev) {
+            canvas.onmousedown = function (ev) {
                 let startX = ev.clientX - this.offsetLeft;
                 let startY = ev.clientY - this.offsetTop;
                 let positionX, positionY;
+                let borderWidth, borderHeight;
+
+                // let border = document.createElement('div');      // 实现边框
+                // border.appendChild(document.createTextNode('border'));
+                // const style = 'position: absolute;border: 1px solid red;text-indent: -9999px;';
 
                 this.onmousemove = function (ev) {
-                    let moveX = ev.clientX;
-                    let moveY = ev.clientY;
+
+                 let moveX = ev.clientX;
+                 let moveY = ev.clientY;
 
                     if (moveX > startX) {
-                        canvas.width = moveX - startX;
+                        borderWidth = moveX - startX;
                         positionX = startX;
-                    }else {
-                        canvas.width = startX - moveX;
+                    } else {
+                        borderWidth = startX - moveX;
                         positionX = moveX;
                     }
 
                     if (moveY > startY) {
-                        canvas.height = moveY - startY;
+                        borderHeight = moveY - startY;
                         positionY = startY;
-                    }else {
-                        canvas.height = startY - moveY;
+                    } else {
+                        borderHeight = startY - moveY;
                         positionY = moveY;
                     }
 
-
-
-
-                    canvas.style = 'position: absolute;' +
-                                    'left: ' + positionX + 'px;' +
-                                    'top: ' + positionY + 'px;' +
-                                    'border: 2px solid red';
-
+                    // 实现边框
+                    // border.style = style + 'width: ' + borderWidth + ';' +
+                    //                         'height: ' + borderHeight + ';' +
+                    //                         'left:' + positionX + ';' +
+                    //                         'top: ' + positionY + ';';
+                    //
+                    // document.body.appendChild(border);
 
                 };
 
+                canvas.onmouseup = function (ev) {
+                    canvas.onmousemove = null;
+                    var mscImage;
+
+                    for (var i = positionX; i < positionX + borderWidth; i = i + size) {
+                        for (var j = positionY; j < positionY + borderHeight; j = j + size) {
+                            mscImage = ctx.getImageData(i, j, size, size);
+                            let colorR = mscImage.data[getCenter(size) * 4];
+                            let colorG = mscImage.data[getCenter(size) * 4 + 1];
+                            let colorB = mscImage.data[getCenter(size) * 4 + 2];
+                            let colorO = mscImage.data[getCenter(size) * 4 + 3];
+
+                            for (var k = 0; k < mscImage.data.length; k++) {
+                                mscImage.data[k * 4] = colorR;
+                                mscImage.data[k * 4 + 1] = colorG;
+                                mscImage.data[k * 4 + 2] = colorB;
+                                mscImage.data[k * 4 + 3] = colorO;
+                            }
+
+                            ctx.putImageData(mscImage, i, j);
+                        }
+                    }
+                }
 
 
                 return false;
             };
 
-            body.onmouseup = function (ev) {
-                body.onmousemove = null;
-
-                var  mscImage;
-
-                debugger
-                for (var i = 0; i < canvas.width; i = i + size){
-                    for (var j = 0; j < canvas.height; j = j + size){
-                        mscImage = ctx.getImageData(i, j, size, size);
-                        let colorR = mscImage.data[getCenter(size) * 4];
-                        let colorG = mscImage.data[getCenter(size) * 4 + 1];
-                        let colorB = mscImage.data[getCenter(size) * 4 + 2];
-                        let colorO = mscImage.data[getCenter(size) * 4 + 3];
-
-                        for (var k = 0; k < mscImage.data.length; k++){
-                            mscImage.data[k * 4] = colorR;
-                            mscImage.data[k * 4 + 1] = colorG;
-                            mscImage.data[k * 4 + 2] = colorB;
-                            mscImage.data[k * 4 + 3] = colorO;
-                        }
-
-                        ctx.putImageData(mscImage, i, j);
-                    }
-                }
-            }
+        }
 
 
 
 
 
 
-
-        // image.onload = function () {
-        //
-        //     canvas.width = image.width * 2;
-        //     canvas.height = image.height;
-        //     ctx.drawImage(image, 0, 0);
 
 
 
